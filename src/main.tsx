9ac22984d1d47e050164b186aeb98a433b979888
Daphne Bogosian <db@kpv-lab.co.uk>
2024-02-07 14:35:42 +0000

    Set up MaterialUI blog template to start work from

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import Blog from './Blog.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Blog />
   </React.StrictMode>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/westofthemoon',
-  plugins: [react()],
+  plugins: [
+    react(),
+    // Custom plugin to load markdown files
+    {
+      name: "markdown-loader",
+      transform(code, id) {
+        if (id.slice(-3) === ".md") {
+          // For .md files, get the raw content
+          return `export default ${JSON.stringify(code)};`;
+        }
+      }
+    }
+  ],
 })
